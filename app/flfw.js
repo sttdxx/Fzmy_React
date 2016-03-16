fc833dedf537d61fcd17d7eb1544183239267f79
sttdxx <842925623@qq.com>
2016-03-16 14:42:15 +0800

    查询 登录

--- app/flfw.js
+++ app/flfw.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBarCPT from './api/NavBarCPT.js';
+import Search from './Search.js';
+import Login from './login.js';
 class Flfw extends React.Component{
 	render(){
 		return (
 			<div>
 				<h1>法律服务</h1>
 				<NavBarCPT index="法律服务" />
+				<Search url="http://www.myflfw.com/law/App/Law/searchLawsNews.action"/>
+				<Login />
 			</div>
 		);
 	}
