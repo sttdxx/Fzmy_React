4632521cbde8fefeb0e6bf0532a5ded81ae5475c
sttdxx <842925623@qq.com>
2016-03-22 15:56:54 +0800

    1

--- app/flfw.js
+++ app/flfw.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { Header } from 'amazeui-react';
 import HeaderFlfw from './storage/HeaderFlfw.js';
 import NavBarCPT from './api/NavBarCPT.js';
-import Search from './Search.js';
-import Login from './login.js';
+
 class Flfw extends React.Component{
 	render(){
 		return (
@@ -11,7 +10,7 @@ class Flfw extends React.Component{
 				<Header {...HeaderFlfw} />
 				<NavBarCPT index="法律服务" />
 				<Search url="http://www.myflfw.com/law/App/Law/searchLawsNews.action"/>
-				<Login />
+				
 			</div>
 		);
 	}
