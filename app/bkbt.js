ba598e2e73c396643e28fe46cea590da21616165
sttdxx <842925623@qq.com>
2016-03-30 20:22:34 +0800

    修改

--- app/bkbt.js
+++ app/bkbt.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Headerbar from './api/Headerbar.js';
-import SearchCPT from './api/SearchCPT.js';
 class Bkbt extends React.Component{
 	render(){
 		return (
 			<div>
 				<Headerbar index="边看边听" link='/bkbt' prev={[{link:'/',icon:'chevron-left'}]}  />
-				<SearchCPT />
 			</div>
 		);
 	}
